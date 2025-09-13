1e112163140580f8cdcc93159ea0aabfd8d9a9d1
Mohammed Hrima <mohammedxhrima@gmail.com>
2025-09-13 16:26:57 +0100

    fix function constant argument

--- src/file.ll
+++ src/file.ll
@@ -1,24 +1,16 @@
 ; ModuleID = '/Users/hrimamohammed/Desktop/Personal/pandu-lang/src/file.pn'
 source_filename = "/Users/hrimamohammed/Desktop/Personal/pandu-lang/src/file.pn"
 
-define i32 @main() {
+define i32 @hello(i32 %0) {
 entry:
   %a = alloca i32, align 4
-  store i32 1, ptr %a, align 4
-  br label %while
-
-while:                                            ; preds = %while_bloc, %entry
   %a1 = load i32, ptr %a, align 4
-  %LESS = icmp slt i32 %a1, 10
-  br i1 %LESS, label %while_bloc, label %end_while
+  ret i32 %a1
+}
 
-while_bloc:                                       ; preds = %while
-  %a2 = load i32, ptr %a, align 4
-  %ADD = add i32 %a2, 1
-  store i32 %ADD, ptr %a, align 4
-  br label %while
-
-end_while:                                        ; preds = %while
-  %a3 = load i32, ptr %a, align 4
-  ret i32 %a3
+define i32 @main() {
+entry:
+  %hello = call i32 @hello(<null operand!>)
+  %a = alloca i32, align 4
+  ret i32 0
 }
--- src/file.s
+++ src/file.s
@@ -1,24 +1,33 @@
 	.build_version macos, 15, 0
 	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_hello                          ; -- Begin function hello
+	.p2align	2
+_hello:                                 ; @hello
+	.cfi_startproc
+; %bb.0:                                ; %entry
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	ldr	w0, [sp, #12]
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
 	.globl	_main                           ; -- Begin function main
 	.p2align	2
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:                                ; %entry
-	sub	sp, sp, #16
-	.cfi_def_cfa_offset 16
-	mov	w0, #1                          ; =0x1
-LBB0_1:                                 ; %while
-                                        ; =>This Inner Loop Header: Depth=1
-	cmp	w0, #9
-	str	w0, [sp, #12]
-	b.gt	LBB0_3
-; %bb.2:                                ; %while_bloc
-                                        ;   in Loop: Header=BB0_1 Depth=1
-	add	w0, w0, #1
-	b	LBB0_1
-LBB0_3:                                 ; %end_while
-	add	sp, sp, #16
+	sub	sp, sp, #32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	.cfi_def_cfa_offset 32
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #1                          ; =0x1
+	add	x0, sp, #12
+	str	w8, [sp, #12]
+	bl	_hello
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
 	ret
 	.cfi_endproc
                                         ; -- End function
