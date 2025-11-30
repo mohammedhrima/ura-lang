8a53c9562ad7b16203aeac6c9684b964455a1740
Mohammed Hrima <mohammedxhrima@gmail.com>
2025-11-30 13:54:09 +0100

    update

--- llvm/out.s
+++ llvm/out.s
@@ -5,13 +5,31 @@
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:                                ; %entry
-	sub	sp, sp, #16
-	.cfi_def_cfa_offset 16
-	mov	w8, #3                          ; =0x3
-	mov	w0, #3                          ; =0x3
-	str	w8, [sp, #12]
-	add	sp, sp, #16
+	sub	sp, sp, #32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	.cfi_def_cfa_offset 32
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+Lloh0:
+	adrp	x8, l_STR0@PAGE
+	ldrsb	w0, [x8, l_STR0@PAGEOFF]
+Lloh1:
+	adrp	x8, l_STR0@PAGE
+Lloh2:
+	add	x8, x8, l_STR0@PAGEOFF
+	str	x8, [sp, #8]
+	strb	w0, [sp, #7]
+	bl	_putchar
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	mov	w0, wzr
+	add	sp, sp, #32
 	ret
+	.loh AdrpAdd	Lloh1, Lloh2
+	.loh AdrpAdrp	Lloh0, Lloh1
 	.cfi_endproc
                                         ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_STR0:                                 ; @STR0
+	.asciz	"abcd"
+
 .subsections_via_symbols
