2aef04e6cfc389be0d8ff7422d35fe4a360009b5
Mohammed Hrima <mohammedxhrima@gmail.com>
2026-01-20 22:16:33 +0100

    clean llvm folders to simpilfy implementing new features

--- llvm/out.ll
+++ llvm/out.ll
@@ -1,79 +1,24 @@
 ; ModuleID = 'module'
 source_filename = "module"
 
-@err_header = private unnamed_addr constant [45 x i8] c"\0Aline %d: segmentation fault (array bounds)\0A\00", align 1
-@err_msg = private unnamed_addr constant [27 x i8] c"error in accessing %s[%d]\0A\00", align 1
-@err_pointer = private unnamed_addr constant [25 x i8] c"                   ^^^^\0A\00", align 1
-
-; Function Attrs: nocallback nofree nosync nounwind willreturn
-declare void @llvm.va_start.p0(ptr) #0
-
-; Function Attrs: nocallback nofree nosync nounwind willreturn
-declare void @llvm.va_end.p0(ptr) #0
-
-define void @bounds_check(i32 %0, i32 %1, i32 %2, i32 %3, ptr %4) {
+define i32 @main() {
 entry:
-  %is_negative = icmp slt i32 %0, 0
-  %is_out_of_bounds = icmp sge i32 %0, %1
-  %is_invalid = or i1 %is_negative, %is_out_of_bounds
-  br i1 %is_invalid, label %error, label %valid
-
-error:                                            ; preds = %entry
-  %5 = call i32 (ptr, ...) @printf(ptr @err_header, i32 %2)
-  %6 = call i32 (ptr, ...) @printf(ptr @err_msg, ptr %4, i32 %0)
-  %7 = call i32 (ptr, ...) @printf(ptr @err_pointer)
-  call void @exit(i32 1)
-  unreachable
-
-valid:                                            ; preds = %entry
-  ret void
-}
-
-declare i32 @printf(ptr, ...)
-
-declare void @exit(i32)
-
-define i32 @sum(i32 %0, ...) {
-entry:
-  %count = alloca i32, align 4
-  store i32 %0, ptr %count, align 4
-  %args = alloca ptr, align 8
-  call void @llvm.va_start.p0(ptr %args)
-  %total = alloca i32, align 4
-  store i32 0, ptr %total, align 4
   %i = alloca i32, align 4
   store i32 0, ptr %i, align 4
   br label %while.cond.0
 
 while.cond.0:                                     ; preds = %while.body.1, %entry
   %i1 = load i32, ptr %i, align 4
-  %count2 = load i32, ptr %count, align 4
-  %lt = icmp slt i32 %i1, %count2
-  br i1 %lt, label %while.body.1, label %while.end.2
+  %less = icmp slt i32 %i1, 5
+  br i1 %less, label %while.body.1, label %while.end.2
 
 while.body.1:                                     ; preds = %while.cond.0
-  %total3 = load i32, ptr %total, align 4
-  %va_arg = va_arg ptr %args, i32
-  %add = add i32 %total3, %va_arg
-  store i32 %add, ptr %total, align 4
-  %i4 = load i32, ptr %i, align 4
-  %add5 = add i32 %i4, 1
-  store i32 %add5, ptr %i, align 4
+  %i2 = load i32, ptr %i, align 4
+  %add = add i32 %i2, 1
+  store i32 %add, ptr %i, align 4
   br label %while.cond.0
 
 while.end.2:                                      ; preds = %while.cond.0
-  call void @llvm.va_end.p0(ptr %args)
-  %total6 = load i32, ptr %total, align 4
-  ret i32 %total6
+  %i3 = load i32, ptr %i, align 4
+  ret i32 %i3
 }
-
-define i32 @main() {
-entry:
-  %result = alloca i32, align 4
-  %sum = call i32 (i32, ...) @sum(i32 3, i32 10, i32 20, i32 30)
-  store i32 %sum, ptr %result, align 4
-  %result1 = load i32, ptr %result, align 4
-  ret i32 %result1
-}
-
-attributes #0 = { nocallback nofree nosync nounwind willreturn }
--- llvm/out.s
+++ llvm/out.s
@@ -1,121 +1,25 @@
 	.build_version macos, 16, 0
 	.section	__TEXT,__text,regular,pure_instructions
-	.globl	_bounds_check                   ; -- Begin function bounds_check
-	.p2align	2
-_bounds_check:                          ; @bounds_check
-	.cfi_startproc
-; %bb.0:                                ; %entry
-                                        ; kill: def $w2 killed $w2 def $x2
-                                        ; kill: def $w0 killed $w0 def $x0
-	tbnz	w0, #31, LBB0_3
-; %bb.1:                                ; %entry
-	cmp	w0, w1
-	b.ge	LBB0_3
-; %bb.2:                                ; %valid
-	ret
-LBB0_3:                                 ; %error
-	sub	sp, sp, #48
-	stp	x20, x19, [sp, #16]             ; 16-byte Folded Spill
-	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
-	.cfi_def_cfa_offset 48
-	.cfi_offset w30, -8
-	.cfi_offset w29, -16
-	.cfi_offset w19, -24
-	.cfi_offset w20, -32
-Lloh0:
-	adrp	x8, l_err_header@PAGE
-Lloh1:
-	add	x8, x8, l_err_header@PAGEOFF
-	mov	x19, x0
-	mov	x0, x8
-	str	x2, [sp]
-	mov	x20, x4
-	bl	_printf
-Lloh2:
-	adrp	x0, l_err_msg@PAGE
-Lloh3:
-	add	x0, x0, l_err_msg@PAGEOFF
-	stp	x20, x19, [sp]
-	bl	_printf
-Lloh4:
-	adrp	x0, l_err_pointer@PAGE
-Lloh5:
-	add	x0, x0, l_err_pointer@PAGEOFF
-	bl	_printf
-	mov	w0, #1                          ; =0x1
-	bl	_exit
-	brk	#0x1
-	.loh AdrpAdd	Lloh4, Lloh5
-	.loh AdrpAdd	Lloh2, Lloh3
-	.loh AdrpAdd	Lloh0, Lloh1
-	.cfi_endproc
-                                        ; -- End function
-	.globl	_sum                            ; -- Begin function sum
-	.p2align	2
-_sum:                                   ; @sum
-	.cfi_startproc
-; %bb.0:                                ; %entry
-	sub	sp, sp, #32
-	.cfi_def_cfa_offset 32
-	mov	w8, wzr
-	add	x9, sp, #32
-	str	w0, [sp, #28]
-	str	x9, [sp, #16]
-	str	wzr, [sp, #12]
-LBB1_1:                                 ; %while.cond.0
-                                        ; =>This Inner Loop Header: Depth=1
-	ldr	w9, [sp, #28]
-	str	w8, [sp, #8]
-	cmp	w8, w9
-	b.ge	LBB1_3
-; %bb.2:                                ; %while.body.1
-                                        ;   in Loop: Header=BB1_1 Depth=1
-	ldr	x8, [sp, #16]
-	add	x9, x8, #8
-	str	x9, [sp, #16]
-	ldp	w10, w9, [sp, #8]
-	ldr	w8, [x8]
-	add	w8, w9, w8
-	str	w8, [sp, #12]
-	add	w8, w10, #1
-	b	LBB1_1
-LBB1_3:                                 ; %while.end.2
-	ldr	w0, [sp, #12]
-	add	sp, sp, #32
-	ret
-	.cfi_endproc
-                                        ; -- End function
 	.globl	_main                           ; -- Begin function main
 	.p2align	2
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:                                ; %entry
-	sub	sp, sp, #48
-	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
-	.cfi_def_cfa_offset 48
-	.cfi_offset w30, -8
-	.cfi_offset w29, -16
-	mov	w8, #30                         ; =0x1e
-	mov	w9, #20                         ; =0x14
-	mov	w0, #3                          ; =0x3
-	stp	x9, x8, [sp, #8]
-	mov	w8, #10                         ; =0xa
-	str	x8, [sp]
-	bl	_sum
-	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
-	str	w0, [sp, #28]
-	add	sp, sp, #48
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	mov	w0, wzr
+LBB0_1:                                 ; %while.cond.0
+                                        ; =>This Inner Loop Header: Depth=1
+	cmp	w0, #4
+	str	w0, [sp, #12]
+	b.gt	LBB0_3
+; %bb.2:                                ; %while.body.1
+                                        ;   in Loop: Header=BB0_1 Depth=1
+	add	w0, w0, #1
+	b	LBB0_1
+LBB0_3:                                 ; %while.end.2
+	add	sp, sp, #16
 	ret
 	.cfi_endproc
                                         ; -- End function
-	.section	__TEXT,__cstring,cstring_literals
-l_err_header:                           ; @err_header
-	.asciz	"\nline %d: segmentation fault (array bounds)\n"
-
-l_err_msg:                              ; @err_msg
-	.asciz	"error in accessing %s[%d]\n"
-
-l_err_pointer:                          ; @err_pointer
-	.asciz	"                   ^^^^\n"
-
 .subsections_via_symbols
