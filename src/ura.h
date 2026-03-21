db89cf377bfc032e36793066503f4a4758657fa9
Mohammed Hrima <mohammedxhrima@gmail.com>
2026-03-21 17:42:45 +0000

    refactoring the code

--- src/ura.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "includes/system.h"
-#include "includes/macros.h"
-#include "includes/types.h"
-#include "includes/globals.h"
-#include "includes/decls.h"
-#include "includes/llvm.h"
