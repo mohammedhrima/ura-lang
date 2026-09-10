cce7b22c4189566b16f1676a1885d19fdd3a97bb
Mohammed Hrima <mhrima@e2r8p10.1337.ma>
2026-09-10 06:19:07 +0100

    implement basic blocs parsing implement Tree printer

--- src/main.c
+++ src/main.c
@@ -21,35 +21,39 @@ typedef struct _IO_FILE *File;
 #define FILE __FILE__
 #define LINE __LINE__
 #define FUNC __func__
+#define TAB  4 /*TODO: to be checked if can be got from some c function*/
 
 #define expand(type, variable) \
     type *variable;            \
-    size_t variable##_count;   \
-    size_t variable##_size;    \
-    size_t variable##_type_size;
+    long variable##_count;     \
+    long variable##_size;
 
-#define push_back(parent, child)                                             \
-    if (parent##_size == 0) /* empty */                                      \
-    {                                                                        \
-        parent##_type_size = sizeof(child);                                  \
-        parent = ura_alloc((parent##_size = 10), parent##_type_size);        \
-    } else if (parent##_count + 1 == parent##_size)                          \
-        parent = realloc(parent, (parent##_size *= 2) * parent##_type_size); \
-    parent[parent##_count++] = child;
+#define push_back(parent, child)                                              \
+    {                                                                         \
+        if (parent##_size == 0) /* empty */                                   \
+            parent = ura_alloc((parent##_size = 10), sizeof(*parent));        \
+        else if (parent##_count + 1 == parent##_size)                         \
+            parent = realloc(parent, (parent##_size *= 2) * sizeof(*parent)); \
+        parent[parent##_count++] = child;                                     \
+    }
 
+
+typedef struct uraFile uraFile;
+typedef enum Type Type;
+typedef struct Token Token;
+typedef struct Node Node;
 
 int _print(File fp, const char *fmt, va_list args);
 int eprint(char *fmt, ...);
+Node *expr_node(int min_op);
 
-typedef struct uraFile uraFile;
 struct uraFile {
     char *filename;
-    size_t len;
+    long len;
     char *content;
 };
 
 // clang-format off
-typedef enum Type Type;
 enum Type {
     NONE,
     IDENTIFIER,
@@ -61,15 +65,17 @@ enum Type {
     ASSIGN,
     ADD, SUB, MUL, DIV, MOD,
 
+    FDEC,
+
     END,
 };
 // clang-format on
 
-typedef struct Token Token;
 struct Token {
     Type type;
 
     bool is_dec;
+    long space;
 
     struct {
         char *name;
@@ -80,7 +86,6 @@ struct Token {
     };
 };
 
-typedef struct Node Node;
 struct Node {
     Token *token;
 
@@ -96,15 +101,15 @@ struct {
     expand(Token *, tokens);
 
     char *curr_content;
-    size_t exe_pos;
+    long exe_pos;
 
-    Node *head;
+    expand(Node *, nodes);
 } ura;
 
 // MEMORY/ERROR/LOGGING HANDLING
 
-void *ura_alloc(size_t count, size_t size) {
-    void *res = calloc(count, sizeof(uraFile));
+void *ura_alloc(long count, long size) {
+    void *res = calloc(count, size);
     if (!res) {
         eprint("ura_alloc failed\n");
     }
@@ -120,9 +125,13 @@ const char *to_string(Type type) {
         [LPARENT] = "LPARENT", [RPARENT] = "LPARENT",
         [DOTS] = "DOTS",
 
+        [ASSIGN] = "ASSIGN",
+
         [ADD] = "ADD", [SUB] = "SUB", [MUL] = "MUL", 
         [DIV] = "DIV", [MOD] = "MOD",
 
+        [FDEC] = "FDEC",
+    
         [END] = "END",
     };
     if (types[type] == NULL)
@@ -140,6 +149,18 @@ int _print(File fp, const char *fmt, va_list args) {
             continue;
         }
 
+        int prefix = 0;
+        if (strncmp(fmt + i, "%i", 2) == 0) {
+            prefix = va_arg(args, int);
+            i += 2;
+            // don't continue
+        }
+
+        if (prefix > 0)
+            r += fprintf(fp, "%*s", prefix, "");
+        if (fmt[i] != '%') /* %i used on its own */
+            continue;
+
 #define macro(value, type)                             \
     if (strncmp(fmt + i, value, strlen(value)) == 0) { \
         r += fprintf(fp, value, va_arg(args, type));   \
@@ -151,6 +172,7 @@ int _print(File fp, const char *fmt, va_list args) {
         macro("%d", int);
         macro("%f", double); // TODO: to be checked
 #undef macro
+
         if (strncmp(fmt + i, "%%", 2) == 0) {
             r += fprintf(fp, "%%");
             i += 2;
@@ -159,12 +181,24 @@ int _print(File fp, const char *fmt, va_list args) {
         if (strncmp(fmt + i, "%k", 2) == 0) {
             Token *token = va_arg(args, Token *);
             r += fprintf(fp, "%s", token ? to_string(token->type) : "(null token)");
+            switch (token->type) {
+            case IDENTIFIER: {
+                r += fprintf(fp, " name (%s)", token->name);
+                break;
+            }
+            case I32: {
+                r += fprintf(fp, " value (%ld)", token->i32.value);
+                break;
+            }
+            default:
+                break;
+            }
+            r += fprintf(fp, " space (%ld)", token->space);
             i += 2;
             continue;
         }
 
         printf("%s:%d unhandled case %s\n", FILE, LINE, fmt + i);
-        // i++;
         break;
     }
     return r;
@@ -187,11 +221,73 @@ int print(char *fmt, ...) {
     return r;
 }
 
+char *format(char *fmt, ...) {
+    char *buf = NULL;
+    size_t size = 0;
+    File out = open_memstream(&buf, &size);
+    if (!out) {
+        eprint("format: open_memstream failed");
+        return NULL;
+    }
+
+    va_list ap;
+    va_start(ap, fmt);
+    _print(out, fmt, ap);
+    va_end(ap);
+    fclose(out);
+    return buf;
+}
+
+// TODO: this code needs to be reviewed
+typedef struct NodePrint NodePrint;
+struct NodePrint {
+    expand(Node *, nodes);
+    expand(int, depths);
+};
+
+void print_helper(NodePrint *elems, Node *node, int depth) {
+    if (!node)
+        return;
+
+    push_back(elems->nodes, node);
+    push_back(elems->depths, depth);
+
+    print_helper(elems, node->left, depth + 2);
+    print_helper(elems, node->right, depth + 2);
+    for (long i = 0; i < node->children_count; i++)
+        print_helper(elems, node->children[i], depth + 2);
+}
+
+int still_open(NodePrint *elems, long i, int level) {
+    for (long j = i + 1; j < elems->nodes_count; j++)
+        if (elems->depths[j] <= level)
+            return elems->depths[j] == level;
+    return 0;
+}
+
+void print_node(Node *node) {
+    NodePrint elems = {};
+
+    print_helper(&elems, node, 0);
+
+    for (long i = 0; i < elems.nodes_count; i++) {
+        int depth = elems.depths[i];
+        for (int level = 1; level < depth; level++)
+            print("%s", still_open(&elems, i, level) ? "│ " : "  ");
+        if (depth)
+            print("%s", still_open(&elems, i, depth) ? "├─" : "└─");
+        print("%k\n", elems.nodes[i]->token);
+    }
+
+    free(elems.nodes);
+    free(elems.depths);
+}
+
 // FILE HANDLING
 uraFile *open_file(char *filename) {
     uraFile *file = ura_alloc(1, sizeof(uraFile));
     if (file == NULL) {
-        eprint("%s:%d", FILE, LINE, "calloc failed");
+        eprint("%s:%d calloc failed\n", FILE, LINE);
         return NULL;
     }
     file->filename = filename;
@@ -199,7 +295,7 @@ uraFile *open_file(char *filename) {
 
     File fp = fopen(filename, "r");
     if (fp == NULL) {
-        eprint("%s:%d", FILE, LINE, "fopen failed");
+        eprint("%s:%d fopen failed\n", FILE, LINE);
         return NULL;
     }
     // TODO: check those to if they failed
@@ -216,15 +312,43 @@ void close_file(uraFile *file) {
     free(file->content);
 }
 
+void free_token(Token *token) {
+    free(token->name);
+    free(token);
+}
+
+void free_node(Node *node) {
+    if (!node)
+        return;
+
+    for (long i = 0; i < node->children_count; i++)
+        free_node(node->children[i]);
+    free(node->children);
+    free_node(node->left);
+    free_node(node->right);
+    free(node);
+}
+
 // TOKENIZE
-Token *new_token(Type type, size_t s, size_t e) {
+Token *new_token(Type type, long space) {
     Token *new = ura_alloc(1, sizeof(Token));
+    new->type = type;
+    new->space = space;
+    push_back(ura.tokens, new);
+    return new;
+}
+
+Token *parse_token(Type type, long s, long e, long space) {
+    Token *new = new_token(type, (space / TAB + (space % TAB != 0 ? 1 : 0)));
     new->type = type;
     switch (type) {
     case IDENTIFIER: {
+        // if(e - s == 0) break;
         if (strncmp(ura.curr_content + s, "i32", e - s) == 0) {
             new->type = I32;
             new->is_dec = true;
+        } else if (strncmp(ura.curr_content + s, "fn", e - s) == 0) {
+            new->type = FDEC;
         } else {
             new->name = ura_alloc(e - s + 1, sizeof(char));
             strncpy(new->name, ura.curr_content + s, e - s);
@@ -240,18 +364,28 @@ Token *new_token(Type type, size_t s, size_t e) {
         break;
     }
     print("new token %k\n", new);
-    push_back(ura.tokens, new);
     return new;
 }
 
 void tokenize(char *content) {
     ura.curr_content = content;
-    size_t s = 0;
-    size_t e = 0;
+    long s = 0;
+    long e = 0;
+    long space = 0;
     while (content && content[e]) {
         s = e;
         if (isspace(content[e])) {
-            e++;
+            while (content[e] == '\n') {
+                space = 0;
+                e++;
+            }
+            if (content[s] == '\n')
+                s = e - 1;
+            while (isspace(content[e]) && content[e] != '\n') {
+                if (content[s] == '\n')
+                    space++;
+                e++;
+            }
             continue;
         }
 
@@ -264,7 +398,7 @@ void tokenize(char *content) {
             e++;
         if (e != s) // found identifier
         {
-            new_token(IDENTIFIER, s, e);
+            parse_token(IDENTIFIER, s, e, space);
             continue;
         }
 
@@ -272,7 +406,7 @@ void tokenize(char *content) {
         while (isdigit(content[e]))
             e++;
         if (e != s) {
-            new_token(I32, s, e);
+            parse_token(I32, s, e, space);
             continue;
         }
         // clang-format off
@@ -282,22 +416,23 @@ void tokenize(char *content) {
         } specials[] = {
             {"(", LPARENT}, {")", RPARENT}, {":", DOTS}, 
             {"+", ADD}, {"-", SUB}, {"*", MUL}, {"/", DIV}, {"%", MOD},
+            {"=", ASSIGN},
             {NULL, NONE}
         };
         // clang-format on
         for (int i = 0; specials[i].value; i++) {
-            size_t len = strlen(specials[i].value);
+            long len = strlen(specials[i].value);
             if (strncmp(specials[i].value, content + e, len) == 0) {
-                new_token(specials[i].type, 0, 0);
+                parse_token(specials[i].type, 0, 0, space);
                 e += len;
             }
         }
         if (e != s)
             continue;
-        eprint("%s:%d handle this case: <%s>", FILE, LINE, content + e);
+        eprint("%s:%d handle this case: <%s>\n", FILE, LINE, content + e);
         break;
     }
-    new_token(END, 0, 0);
+    parse_token(END, 0, 0, 0);
 }
 
 // ABSTRACT SYNTAX TREE
@@ -312,6 +447,10 @@ Token *find(Type type, ...) {
     return NULL;
 }
 
+bool inside(long space) {
+    return ura.tokens[ura.exe_pos]->space > space && ura.errors_count == 0;
+}
+
 Token *peek(int index) {
     if (ura.exe_pos + index > ura.tokens_count) {
         eprint("%s:%d index out of range\n", FILE, LINE);
@@ -324,7 +463,7 @@ Token *expect(Type type) {
     return NULL;
 }
 
-Token *next() {
+Token *next() { // TODO: protect if next is END, don't advanced
     Token *token = peek(0);
     ura.exe_pos++;
     return token;
@@ -339,12 +478,43 @@ Node *new_node(Token *token) {
 
 Node *prime_node() {
     Token *token = next();
+    Node *node = NULL;
     switch (token->type) {
-    case I32: {
+    case I32:
+    case IDENTIFIER: {
         return new_node(token);
     }
-    default: {
-        eprint("%s:%d handle this case", FILE, LINE);
+    case FDEC: {
+        node = new_node(token);
+        if (peek(0)->type != IDENTIFIER) {
+            eprint("Expected identifer after fn\n");
+            free_node(node);
+            return NULL;
+        }
+        node->token->name = strdup(next()->name);
+
+        if (next()->type != LPARENT)
+            eprint("Expected ( after function declaration\n");
+        if (next()->type != RPARENT)
+            eprint("Expected ) after function declaration\n");
+
+        if (next()->type != DOTS)
+            eprint("Expected : after function declaration\n");
+
+        while (inside(node->token->space)) {
+            push_back(node->children, expr_node(0));
+        }
+        return node;
+    }
+    case LPARENT: {
+        next(); // skip (
+        node = expr_node(0);
+        if (next()->type != RPARENT)
+            eprint("Expected )\n");
+        return node;
+    }
+    default: { // TODO: replace this with unexpected token
+        eprint("%s:%d handle this case %s\n", FILE, LINE, to_string(token->type));
         break;
     }
     }
@@ -356,13 +526,15 @@ Node *expr_node(int min_op) {
     while (true) {
         // clang-format off
         int prec[END + 1] = {
+            [ASSIGN] = 1,
             [ADD] = 10, [SUB] = 10, 
             [MUL] = 11, [DIV] = 11, [MOD] = 11,
         };
         // clang-format on
         Type type = peek(0)->type;
+        // if(type == END) break;
         int op = prec[type];
-        if (op < min_op)
+        if (op <= min_op)
             break;
         Node *node = new_node(next());
         node->left = left;
@@ -373,15 +545,14 @@ Node *expr_node(int min_op) {
     return left;
 }
 
+
 void generate_ast() {
     if (ura.errors_count)
         return;
-    ura.head = new_node(new_token(IDENTIFIER, 0, 0));
-    while(!find(END, 0) && !ura.errors_count) {
+    while (!find(END, 0) && !ura.errors_count) {
         Node *child = expr_node(0);
-        push_back(ura.head->children, child);
+        push_back(ura.nodes, child);
     }
-
 }
 
 /*
@@ -396,6 +567,13 @@ int main() {
     tokenize(file->content);
     print("============AST=================\n");
     generate_ast();
+    print("=========PRINT AST==============\n");
+    for (long i = 0; i < ura.nodes_count; i++)
+        print_node(ura.nodes[i]);
     print("============Cleaning============\n");
+    for (long i = 0; i < ura.nodes_count; i++)
+        free_node(ura.nodes[i]);
+    for (long i = 0; i < ura.tokens_count; i++)
+        free_token(ura.tokens[i]);
     close_file(file);
 }
